09a65cfd021bd60f5402b7b1a0e24efb39c04254
prajjwal98dubey <prajjwaldubey003@gmail.com>
2024-12-02 21:28:17 +0530

    day1 part2 commit

--- src/day1.py
+++ src/day1.py
@@ -1,3 +1,4 @@
+import collections
 def solve(l1,l2):
     l1.sort()
     l2.sort()
@@ -5,6 +6,15 @@ def solve(l1,l2):
     for i in range(len(l1)):
         ans+=abs(l1[i]-l2[i])
     return ans
+def findSimilarityScore(l1,l2):
+    count_of_numbers_in_right_list = collections.defaultdict(int)
+    for n in l2:
+        count_of_numbers_in_right_list[n]+=1
+    res = 0
+    for num in l1:
+        res += count_of_numbers_in_right_list[num]*num
+    return res
+        
 def main():
     list1 = []
     list2 = []
@@ -16,7 +26,10 @@ def main():
             list1_element,list2_element = map(int,line.split())
             list1.append(list1_element)
             list2.append(list2_element)
+    # for day1 part1 run solve function and comment out the findSimilarityScore function call.
     ans = solve(list1,list2)
+    #for day1 part2 run findSimilarityScore function and comment out the solve function call.
+    ans = findSimilarityScore(list1,list2)
     return ans
 print(main())
 
